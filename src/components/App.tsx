ec643f8c76e33333379eea5c206b77dedd4f3442
Smit Barmase <smitbarmase@outlook.com>
2021-03-03 03:16:46 +0530

    route fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
   };
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className={theme}>
         <Layout>
           <Navbar handleTheme={handleTheme} />
